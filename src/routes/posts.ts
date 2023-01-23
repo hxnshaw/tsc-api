8b25021fe00a03bd8d0a5199e9cc9d696d8c496c
hxnshaw <eghosasere07@gmail.com>
2023-01-23 12:04:12 +0100

    bug fix

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -6,6 +6,6 @@ router.get('/posts',controller.getPosts)
 router.get('/posts/:id',controller.getPost)
 router.put('/posts/:id',controller.updatePost)
 router.delete('/posts/:id',controller.deletePost)
-router.post('/posts/:id',controller.addPost)
+router.post('/posts',controller.addPost)
 
 export=router;
