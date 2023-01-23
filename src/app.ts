64865184d4ac8ba7cbfae6b88178fe1147fca244
hxnshaw <eghosasere07@gmail.com>
2023-01-23 11:04:46 +0100

    new update

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
- console.log("hello world");
- console.log("testing");
+import express,{Request,Response} from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+//app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req:Request, res:Response) =>  {
+    return res.send("Hello world")
+})
  
+
+app.listen(3000,()=>{
+    console.log("App is listening on port 3000");
+    
+})   
